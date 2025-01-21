a9496a4148efef7692b013d122b329f1b2031c82
Rbaptiste <baptiste.renou@ynov.com>
2025-01-21 16:57:46 +0100

    instruments

---
+++ Musique-generator/Instrument.cpp
@@ -0,0 +1,5 @@
+#include "Instrument.hpp"
+
+Instrument::Instrument()
+{
+}
--- Musique-generator/Instrument.hpp
+++ Musique-generator/Instrument.hpp
@@ -1,7 +1,18 @@
 #ifndef INSTRUMENT
 #define INSTRUMENT
 
+#include <iostream>
+using namespace std;
+
 class Instrument 
-{ };
+{ 
+protected:
+	string m_nom;
+	string m_vitessse;
+
+public:
+	Instrument();
+
+};
 
 #endif
