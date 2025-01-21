34cde36d0e28a6d076e1c7ed25e2375ce2b9b93f
cjohann <johann.chiarotto@ynov.com>
2025-01-21 16:33:32 +0100

    ajout fin

--- Musique-generator/Musique-generator.cpp
+++ Musique-generator/Musique-generator.cpp
@@ -4,8 +4,5 @@ using namespace std;
 
 int main()
 {
-    cout << "coucou on est sur le main";
-    cout << "ca commence me peter les boules";
-    cout << "c'est un peut chiant cette histoire";
-    cout << "maintenant c'est au tour d'adrien";
+    cout << "coucou on est sur le main" << endl;
 }
