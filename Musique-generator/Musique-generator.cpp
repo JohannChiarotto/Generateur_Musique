c368df239f18a070b75ee41c586cb4c3492a2dda
duadrien <adrien.dulou@ynov.com>
2025-01-21 16:32:41 +0100

    peux etre la fin

--- Musique-generator/Musique-generator.cpp
+++ Musique-generator/Musique-generator.cpp
@@ -7,4 +7,5 @@ int main()
     cout << "coucou on est sur le main";
     cout << "ca commence me peter les boules";
     cout << "c'est un peut chiant cette histoire";
+    cout << "maintenant c'est au tour d'adrien";
 }
