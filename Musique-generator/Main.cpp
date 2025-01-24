738e0609ba64d72b2688f8b8c45862904fc0227f
cjohann <johann.chiarotto@ynov.com>
2025-01-24 14:17:42 +0100

    ajout debut du main

---
+++ Musique-generator/Main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Bonjour et bienvenue sur notre simulateur interactif de musique." << endl;
+    cout << "Vous avez le choix entre :" << endl << "1. Jouer de la musique" << endl << "2. Jouer une partition deja enregistree" << endl;
+    int choix1;
+    cin >> choix1;
+
+    if (choix1 == 1) //Jouer un intrument
+    {
+        cout << endl << "Vous pouvez jouez un instrument parmis les suivants :" << endl;
+        cout << "1. Guitare" << endl << "2. Piano" << endl << "3. Harpe" << endl;
+        int instrument;
+        cin >> instrument;
+
+        if (instrument == 1) //Guitare
+        {
+            system("cls");
+            cout << "Vous avez choisi la guitare." << endl;
+        }
+        else if (instrument == 2) //Piano
+        {
+            system("cls");
+            cout << "Vous avez choisi la guitare." << endl;
+        }
+        else if (instrument == 3) //Harpe
+        {
+            system("cls");
+            cout << "Vous avez choisi la guitare." << endl;
+        }
+        else { //Erreur
+            cout << "Il y a une erreur dans le choix de l'intruement" << endl;
+        }
+
+
+    }
+    else if (choix1 == 2) //Jouer une partition
+    {
+
+    }
+    else //Erreur
+    {
+        cout << "Erreur lors de la selection !" << endl;
+    }
+}
